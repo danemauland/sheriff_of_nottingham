7a36294be64d0112c6614acc65d165ce9e6b2be6
dane <danemauland@gmail.com>
2020-11-17 09:13:22 -0500

    beginning auth

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,23 @@
+class Api::SessionsController < ApplicationController
+    def create
+        @user = User.find_by_credentials(
+            params[:user][:username],
+            params[:user][:password])
+        if @user
+            login(@user)
+            render "api/users/show"
+        else
+            render json: ["Wrong username or password"], status: 422
+        end
+    end
+
+    def destroy
+        if logged_in?
+            logout!
+            render json: {}
+        else
+            render json: ["Not logged in"], status: 404 unless logged_in?
+        end
+    end
+
+end
