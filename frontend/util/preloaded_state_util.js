d2c4898b8189e97fe190d09d274a7dad01b237f2
dane <danemauland@gmail.com>
2021-01-05 12:55:31 -0500

    Add comments to preloaded state util

--- frontend/util/preloaded_state_util.js
+++ frontend/util/preloaded_state_util.js
@@ -5,13 +5,26 @@ import {
 } from "./new_entities_util";
 var merge = require('lodash.merge');
 
-export default (user) => {
+export default user => {
+
     if (!user) return undefined;
-    const trades = [...user.trades].sort((a, b) => a.createdAt - b.createdAt);
-    const cashTransactions = [...user.cashTransactions].sort((a, b) => a.createdAt - b.createdAt);
+
+    const sortOrder = (a, b) => a.createdAt - b.createdAt;
+
+    // trades/cash transactions are received in the order they were added to
+    // the database, so back-dated transactions will be out of order
+    const trades = [...user.trades].sort(sortOrder);
+    const cashTransactions = [...user.cashTransactions].sort(sortOrder);
+
+    // calculates stocks owned at any given point in time based off the trades
     const ownershipHistories = getOwnershipHistories(trades);
+    
     const tickers = new Set(Object.keys(ownershipHistories.numShares));
+
+    // calculates the cash balance at any given time based off deposits/
+    // withdrawals and cash spent on trades
     const cashHistory = getCashHistoy(cashTransactions, trades);
+
     let newEntities = {
         assetInformation: {
             tickers,
@@ -23,12 +36,16 @@ export default (user) => {
             trades,
         },
     };
+
+    // adds in all default newEnities keys to newEntities object
     newEntities = merge({}, defaultState, newEntities);
+
     const preloadedState = {
         session: {
             username: user.username,
         },
         newEntities,
     }
+
     return preloadedState;
 }
