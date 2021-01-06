d4fda67b71f9ee0386481f5a8df2f24e83d3ef43
dane <danemauland@gmail.com>
2021-01-06 10:15:14 -0500

    change hover link from previous commit to be consistent with rest of app

--- frontend/components/splash/nav_links.jsx
+++ frontend/components/splash/nav_links.jsx
@@ -42,27 +42,27 @@ class NavLinks extends React.Component {
                     <Logo />
                     <div className="dropdowns small-menu hidden-1024">
                         <span>
-                            <button className="green-hover" onClick={this.addClickHandler("products")}>Products {this.state.selected === "products" ? <CgChevronUp /> : <CgChevronDown />}</button>
+                            <button className="dark-green-hover" onClick={this.addClickHandler("products")}>Products {this.state.selected === "products" ? <CgChevronUp /> : <CgChevronDown />}</button>
                             <ul className="product-list">
-                                <li className="green-hover"><a href="#">Stocks &amp; Funds</a></li>
-                                <li className="green-hover"><a href="#">Options</a></li>
-                                <li className="green-hover"><a href="#">Gold</a></li>
-                                <li className="green-hover"><a href="#">Cash Management</a></li>
+                                <li className="dark-green-hover"><a href="#">Stocks &amp; Funds</a></li>
+                                <li className="dark-green-hover"><a href="#">Options</a></li>
+                                <li className="dark-green-hover"><a href="#">Gold</a></li>
+                                <li className="dark-green-hover"><a href="#">Cash Management</a></li>
                                 <li className="crypto-li"><a href="#">Crypto</a></li>
                             </ul>
                         </span>
                         <span>
-                            <button className="green-hover" onClick={this.addClickHandler("learn")}>Learn  {this.state.selected === "learn" ? <CgChevronUp /> : <CgChevronDown />}</button>
+                            <button className="dark-green-hover" onClick={this.addClickHandler("learn")}>Learn  {this.state.selected === "learn" ? <CgChevronUp /> : <CgChevronDown />}</button>
                             <ul className="learn-list">
-                                <li className="green-hover"><a href="#">Snacks</a></li>
-                                <li className="green-hover"><a href="#">Learn</a></li>
+                                <li className="dark-green-hover"><a href="#">Snacks</a></li>
+                                <li className="dark-green-hover"><a href="#">Learn</a></li>
                             </ul>
                         </span>
                         <span>
-                            <button className="green-hover" onClick={this.addClickHandler("about")}>About Me {this.state.selected === "about" ? <CgChevronUp /> : <CgChevronDown />}</button>
+                            <button className="dark-green-hover" onClick={this.addClickHandler("about")}>About Me {this.state.selected === "about" ? <CgChevronUp /> : <CgChevronDown />}</button>
                             <ul className="about-list">
-                                <li className="green-hover"><a target="_blank" href="https://www.linkedin.com/in/dane-m-63a34412a/">LinkedIn</a></li>
-                                <li className="green-hover"><a target="_blank" href="https://github.com/danemauland">Github</a></li>
+                                <li className="dark-green-hover"><a target="_blank" href="https://www.linkedin.com/in/dane-m-63a34412a/">LinkedIn</a></li>
+                                <li className="dark-green-hover"><a target="_blank" href="https://github.com/danemauland">Github</a></li>
                             </ul>
                         </span>
                     </div>
