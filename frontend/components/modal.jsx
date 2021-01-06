89f0c252e6875da3326e0996114ea810dd70526d
dane <danemauland@gmail.com>
2021-01-06 13:36:24 -0500

    refactor modal

--- frontend/components/modal.jsx
+++ frontend/components/modal.jsx
@@ -4,32 +4,32 @@ import { connect } from "react-redux";
 import CommissionPrompt from "./splash/commission/commission_prompt";
 import { GrClose } from "react-icons/gr";
 
-class Modal extends React.Component {
-    constructor(props) {
-        super(props);
+const Modal = ({modal, closeModal}) => {
+    let component;
+
+    switch (modal) {
+        case "commission":
+            component = <CommissionPrompt />;
+            break;
+
+        default:
+            return null;
     }
 
-    render() {
-        if (!this.props.modal) {return null}
-        let component;
-        switch (this.props.modal) {
-            case "commission":
-                component = <CommissionPrompt />;
-                break;
-            default:
-                return null;
-        }
-        return (
-            <div className="modal-background" onClick={this.props.closeModal}>
-                <div className="modal-child" onClick={e => e.stopPropagation()}>
-                    <div className="x-container">
-                        <button onClick={this.props.closeModal}><GrClose /></button>
-                    </div>
-                    {component}
+    return (
+        <div className="modal-background" onClick={closeModal}>
+            <div className="modal-child" onClick={e => e.stopPropagation()}>
+
+                <div className="x-container">
+                    <button onClick={closeModal}>
+                        <GrClose />
+                    </button>
                 </div>
+
+                {component}
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 const mapStateToProps = state => ({
