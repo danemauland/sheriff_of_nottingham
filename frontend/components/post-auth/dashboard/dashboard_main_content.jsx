3e5a5d9f59c0bc428cc9b97588425c176d9f81cf
dane <danemauland@gmail.com>
2021-01-07 16:21:41 -0500

    Rename MarketNews to MarketNewsContainer

--- frontend/components/post-auth/dashboard/dashboard_main_content.jsx
+++ frontend/components/post-auth/dashboard/dashboard_main_content.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import AccountChartContainer from "./account_chart_container";
 import Cash from "./cash";
-import MarketNews from "./market_news";
+import MarketNewsContainer from "./market_news_container";
 
 const DashboardMainContent = () => (
     <div className="dashboard-main-content">
         <AccountChartContainer />
         <Cash />
-        <MarketNews />
+        <MarketNewsContainer />
     </div>
 )
 
