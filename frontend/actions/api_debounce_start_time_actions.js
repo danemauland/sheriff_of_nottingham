5a063fbb9fe740c3458eaabf0af6ed22202e469c
dane <danemauland@gmail.com>
2021-01-07 09:11:19 -0500

    Minor refactor to api debounce actions

--- frontend/actions/api_debounce_start_time_actions.js
+++ frontend/actions/api_debounce_start_time_actions.js
@@ -1,7 +1,7 @@
 export const SET_API_DEBOUNCE_START_TIME = "SET_API_DEBOUNCE_START_TIME";
 export const REMOVE_API_DEBOUNCE_START_TIME = "REMOVE_API_DEBOUNCE_START_TIME";
 
-export const setAPIDebounceStartTime = (startTime) => ({
+export const setAPIDebounceStartTime = startTime => ({
     type: SET_API_DEBOUNCE_START_TIME,
     startTime,
 })
