862ff4c3a05617eda3f1ecdd04faab903b47ec2f
dane <danemauland@gmail.com>
2021-01-07 16:18:53 -0500

    minor formatting changes to marketNews code

--- frontend/components/post-auth/dashboard/market_news.js
+++ frontend/components/post-auth/dashboard/market_news.js
@@ -2,14 +2,13 @@ import {connect} from "react-redux";
 import News from "../news/news"
 import {fetchMarketNews} from "../../../actions/external_api_actions";
 
-const mapStateToProps = ({newEntities : {marketNews}}) => {
-    return ({
+const mapStateToProps = ({newEntities : {marketNews}}) => ({
     news: marketNews,
     hasReceivedNews: !!marketNews.length,
-})}
+});
 
 const mapDispatchToProps = dispatch => ({
     fetchNews: () => fetchMarketNews(dispatch),
-})
+});
 
 export default (connect(mapStateToProps, mapDispatchToProps)(News));
