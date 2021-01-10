7b15bce8b5f9fe8450a4fa013a8e36c50201d7fc
dane <danemauland@gmail.com>
2021-01-09 20:55:39 -0500

    DRY up state extraction functions

--- frontend/util/extract_from_state_utils.js
+++ frontend/util/extract_from_state_utils.js
@@ -9,44 +9,78 @@ const getAssetInformation = state => getNewEntities(state).assetInformation;
 
 const getAssetValuations = state => getAssetInformation(state).valuations;
 
+const getOwnershipHistories = state => (
+    getAssetInformation(state).ownershipHistories
+);
+
+const getSharesHistory = state => getOwnershipHistories(state).numShares;
+
+const getPosSharesHistory = (ticker, state) => getSharesHistory(state)[ticker];
+
+const getOneDayPortfolioValuations = state => (
+    getPortfolioValuations(state).oneDay
+);
+
 const getOneDayAssetValuations = state => getAssetValuations(state).oneDay;
 
+const getOneYearAssetValuations = state => getAssetValuations(state).oneYear;
+
+const getPosOneDayValuations = (ticker, state) => (
+    getOneDayAssetValuations(state)[ticker]
+);
+
+const getPosOneYearValuations = (ticker, state) => (
+    getOneYearAssetValuations(state)[ticker]
+);
+
+const getPosCosts = state => getAssetInformation(state).positionCosts;
+
+const getPortfolioHistory = state => getNewEntities(state).portfolioHistory;
+
+const getCashHistory = state => getPortfolioHistory(state).cashHistory;
+
+const getValuationHistory = state =>getPortfolioHistory(state).valuationHistory;
+
+const getCashBalances = state => getCashHistory(state).balances;
+
+const getCashTimes = state => getCashHistory(state).times;
+
+const getUI = state => state.ui;
+
 export const getPosMarketValue = (ticker, state) => (
-    getOneDayAssetValuations(state)[ticker].last()
+    getPosOneDayValuations(ticker, state).last()
 );
 
-export const getSharesOwned = (ticker, {newEntities}) => (
-    newEntities.assetInformation.ownershipHistories.numShares[ticker].last()
+export const getSharesOwned = (ticker, state) => (
+    getPosSharesHistory(ticker, state).last()
 );
 
-export const getPosCost = (ticker, {newEntities}) => (
-    newEntities.assetInformation.positionCosts[ticker]
+export const getPosCost = (ticker, state) => getPosCosts(state)[ticker];
+
+export const getPrevDayClose = (ticker, state) => (
+    getPreviousEndingValue(getPosOneYearValuations(ticker, state), ONE_DAY)
 );
 
-export const getPrevDayClose = (ticker, {newEntities: {assetInformation}}) => (
-    getPreviousEndingValue(assetInformation.valuations.oneYear[ticker], ONE_DAY)
+export const getPortfolioValue = state => (
+    getOneDayPortfolioValuations(state).last()
 );
 
-export const getPortfolioValue = (state) => (
-    getPortfolioValuations(state).oneDay.last()
+export const getStartingCashBal = state => (
+    getCashBalances(state)[0]
 );
 
-export const getStartingCashBal = ({newEntities}) => (
-    newEntities.portfolioHistory.cashHistory.balances[0]
+export const getStartingCashTime = state => (
+    getCashTimes(state)[0]
 );
 
-export const getStartingCashTime = ({newEntities}) => (
-    newEntities.portfolioHistory.cashHistory.times[0]
+export const getPortfolioValuations = state => (
+    getValuationHistory(state).valuations
 );
 
-export const getPortfolioValuations = ({newEntities}) => (
-    newEntities.portfolioHistory.valuationHistory.valuations
+export const getPortfolioValuationsTimes = state => (
+    getValuationHistory(state).times
 );
 
-export const getPortfolioValuationsTimes = ({newEntities}) => (
-    newEntities.portfolioHistory.valuationHistory.times
-);
-
-export const getValueIncreased = (state) => (
-    state.ui.valueIncreased
+export const getValueIncreased = state => (
+    getUI(state).valueIncreased
 );
