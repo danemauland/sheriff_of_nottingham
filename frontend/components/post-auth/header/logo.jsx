82663de904283b57383bdc9952cb4dacfef4d748
dane <danemauland@gmail.com>
2021-01-11 15:09:26 -0500

    Refactor header Logo to depend on state extraction method

--- frontend/components/post-auth/header/logo.jsx
+++ frontend/components/post-auth/header/logo.jsx
@@ -2,13 +2,11 @@ import React from "react";
 import { GiPoliceBadge } from "react-icons/gi";
 import {Link} from "react-router-dom";
 import { connect } from "react-redux";
+import {
+    getValueIncreased,
+} from "../../../util/extract_from_state_utils";
 
-const mapStateToProps = state => {
-    return ({
-        valueIncreased: state.ui.valueIncreased,
-    })
-}
-
+const mapStateToProps = state => ({valueIncreased: getValueIncreased(state)});
 
 const Logo = props => (
     <Link to="/dashboard" className={"dashboard-logo-link " + (props.valueIncreased ? "dark-green-hover" : "red-hover")}>
