e26d44683e5e0168915a68c9c63bcb19bb79f474
dane <danemauland@gmail.com>
2021-01-11 15:11:27 -0500

    Refactor SearchResultItem to depend on state extraction method

--- frontend/components/post-auth/header/search_result_item.jsx
+++ frontend/components/post-auth/header/search_result_item.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { connect } from "react-redux";
 import {Link} from "react-router-dom";
+import {
+    getValueIncreased,
+} from "../../../util/extract_from_state_utils";
 
-const mapStateToProps = state => ({
-    valueIncreased: state.ui.valueIncreased,
-})
+const mapStateToProps = state => ({valueIncreased: getValueIncreased(state)});
 
 class SearchResultItem extends React.Component {
     constructor(props) {
