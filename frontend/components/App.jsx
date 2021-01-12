f9d18c6b0943c0aff6466afd0239ef5bd410b47b
dane <danemauland@gmail.com>
2021-01-12 11:13:22 -0500

    Move Initialize component functionality to App

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,17 +1,50 @@
 import React from "react";
-import Initialize from "./initialize";
 import {connect} from "react-redux";
 import Modal from "./modal.jsx";
 import PostAuth from "./post-auth/post-auth";
 import PreAuth from "./pre-auth/pre-auth";
 
-const App = ({isLoggedIn}) => (
-    <>
-        <Modal />
-        <Initialize isLoggedIn={isLoggedIn}/>
-        {isLoggedIn ? <PostAuth /> : <PreAuth />}
-    </>
-)
+
+class App extends React.Component {
+
+    componentDidUpdate(prevProps) {
+        const isLoggedIn = this.props.isLoggedIn;
+
+        let wasLoggedIn;
+        if (!prevProps) {
+            wasLoggedIn = !isLoggedIn;
+            const meta = document.createElement("meta");
+            
+            meta.setAttribute("name", "color-scheme");
+
+            document.getElementsByTagName("head")[0].appendChild(meta);
+        } else wasLoggedIn = prevProps.isLoggedIn;
+
+        if (isLoggedIn !== wasLoggedIn) {
+            const tag = $('meta[name="color-scheme"]');
+            const body = $("body");
+
+            if (isLoggedIn) {
+                tag.attr("content", "dark");
+                body.addClass("dark");
+            } else {
+                tag.attr("content", "light"); 
+                body.removeClass("dark");
+            }
+        }
+    }
+
+    componentDidMount() {
+        this.componentDidUpdate();
+    }
+
+    render() {
+        return (<>
+            <Modal />
+            {this.props.isLoggedIn ? <PostAuth /> : <PreAuth />}
+        </>)
+    }
+}
 
 const mapStateToProps = state => ({
     isLoggedIn: !!state.session.username,
