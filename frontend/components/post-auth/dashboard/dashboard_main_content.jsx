02ed3e067694be2abb571eb3acecb1f80bb8bb1e
dane <danemauland@gmail.com>
2021-01-12 11:53:31 -0500

    rename AccountChartContainer to DashboardChartContainer

--- frontend/components/post-auth/dashboard/dashboard_main_content.jsx
+++ frontend/components/post-auth/dashboard/dashboard_main_content.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import AccountChartContainer from "./account_chart_container";
+import DashboardChartContainer from "./dashboard_chart_container";
 import Cash from "./cash";
 import MarketNewsContainer from "./market_news_container";
 
 const DashboardMainContent = () => (
     <div className="post-auth-main-content">
-        <AccountChartContainer />
+        <DashboardChartContainer />
         <Cash />
         <MarketNewsContainer />
     </div>
