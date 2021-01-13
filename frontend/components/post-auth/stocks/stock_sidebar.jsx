d281a033eafd4b12b38e951e5e6135e534f4854c
dane <danemauland@gmail.com>
2021-01-13 11:03:01 -0500

    Refactoring StockSidebar

--- frontend/components/post-auth/stocks/stock_sidebar.jsx
+++ frontend/components/post-auth/stocks/stock_sidebar.jsx
@@ -7,7 +7,11 @@ import {
     getValueIncreased,
 } from "../../../util/extract_from_state_utils";
 
+const DIGIT_STRINGS = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
 const mapStateToProps = (state, {ticker}) => ({
+    colorDark: getValueIncreased(state, ticker) ? "dark-green" : "red",
+    color: getValueIncreased(state, ticker) ? "green" : "red",
     valueIncreased: getValueIncreased(state, ticker),
     price: getLastPrice(state, ticker),
 });
@@ -20,8 +24,8 @@ class Sidebar extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            order: "Buy",
-            investIn: "Dollars",
+            isBuyOrder: true,
+            isDollarOrder: true,
             amount: "",
             numDecimals: 0,
             numShares: "",
@@ -35,26 +39,30 @@ class Sidebar extends React.Component {
         this.handleChange = this.handleChange.bind(this);
         this.handleBlur = this.handleBlur.bind(this);
         this.handleReset = this.handleReset.bind(this);
-        this.graphClickListener = this.graphClickListener.bind(this);
+        // this.graphClickListener = this.graphClickListener.bind(this);
     }
 
-    componentDidMount() {
-        document.getElementById("myChart").addEventListener("click", this.graphClickListener)
-    }
+    // TODO: REFACTOR AS EVENT HANDLER PASSED TO CHART THAT DISPATCHES TO STORE
+    // ON CLICK
 
-    graphClickListener(e) {
-        const lineChart = $("#myChart").data("myChart");
-        const datasetIndex = lineChart.tooltip._active[0]._datasetIndex;
-        const index = lineChart.tooltip._active[0]._index;
-        const price = lineChart.tooltip._data.datasets[datasetIndex].data[index];
-        const createdAt = lineChart.tooltip._data.datasets[4].data[index];
-        this.setState({price: price*100,createdAt: createdAt * 1000});
-    }
+    // componentDidMount() {
+    //     document.getElementById("myChart").addEventListener("click", this.graphClickListener)
+    // }
+
+    // graphClickListener(e) {
+    //     const lineChart = $(e.target)
+    //     // .data("myChart");
+    //     const datasetIndex = lineChart.tooltip._active[0]._datasetIndex;
+    //     const index = lineChart.tooltip._active[0]._index;
+    //     const price = lineChart.tooltip._data.datasets[datasetIndex].data[index];
+    //     const createdAt = lineChart.tooltip._data.datasets[4].data[index];
+    //     this.setState({price: price*100,createdAt: createdAt * 1000});
+    // }
 
     resetState() {
         this.setState({
-            order: "Buy",
-            investIn: "Dollars",
+            isBuyOrder: true,
+            isDollarOrder: true,
             amount: "",
             numDecimals: 0,
             numShares: "",
@@ -64,76 +72,78 @@ class Sidebar extends React.Component {
     }
 
     componentDidUpdate(prevProps) {
-        if (prevProps.location !== this.props.location) {
-            this.resetState();
-        }
+        if (prevProps.location !== this.props.location) this.resetState();
     }
 
     generateClassName(field) {
         let className = "";
-        if (field === this.state.order) {
-            className += "selected "
-            if (this.props.valueIncreased) {
-                className += "dark-green ";
-            } else {
-                className += "red ";
-            }
-        } 
-        if (this.props.valueIncreased) {
-            className += "dark-green-hover "
-        } else {
-            className += "red-hover "
-        }
+        const colorDark = this.props.colorDark;
+        const isBuyOrder = this.state.isBuyOrder;
+        
+        const isSelected = (field === "Buy") === isBuyOrder;
+
+        if (isSelected) className += `selected ${colorDark} `; 
+        
+        className += `${colorDark}-hover `;
+
         return className;
     }
 
     handleBuy(e) {
         e.preventDefault();
+
         this.setState({
-            order: "Buy",
+            isBuyOrder: true,
         })
     }
 
     handleSell(e) {
         e.preventDefault();
+
         this.setState({
-            order: "Sell",
+            isBuyOrder: false,
         })
     }
 
     handleSelect(e) {
-        if (e.target.value !== this.props.investIn) {
+        const wasDollarOrder = this.state.isDollarOrder;
+        const isDollarOrder = e.target.value === "Dollars";
+
+        if (wasDollarOrder !== isDollarOrder) {
             this.setState({
-                investIn: e.target.value,
+                isDollarOrder,
             })
         }
+    }
+
+    handleDollarOrderChange(val) {
+        let numDecimals = -1;
+        let numStr = "";
+        for(let char of val) {
+            if (DIGIT_STRINGS.includes(char) || char === ".") {
+                if (numDecimals === -1 || char !== ".") {
+                    numStr += char;
+
+                    const numDecIs0or1 = (numDecimals > -1 && numDecimals < 2);
+                    if (numDecIs0or1 || char === ".") numDecimals++;
+                }
+            }
+        }
+        if (numStr === "$" || numStr === "") {
+            this.setState({amount: ""});
+            return;
+        }
+        
+        const amount = Math.floor(parseFloat(numStr) * 100);
+        this.setState({amount: amount, numDecimals})
     }
 
     handleChange(e) {
         let allowableChars;
         const val = e.target.value;
         let numStr = "";
-        if (this.state.investIn === "Dollars") {
-            allowableChars = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
-            let numDecimals = 0;
-            for(let i = 0; i < val.length; i++) {
-                if (allowableChars.includes(val[i])) {
-                    if (numDecimals === 0) {
-                        numStr += val[i]
-                        if (val[i] === ".") {numDecimals = -1}
-                    } else if (val[i] !== ".") {
-                        numStr += val[i]
-                        if (numDecimals === -1) {
-                            numDecimals = 1;
-                        } else if (numDecimals === 1) {
-                            numDecimals = 2;
-                        }
-                    }
-                }
-            }
-            if (numStr === "$" || numStr === "") {this.setState({amount: ""}); return;}
-            const amount = Math.floor(parseFloat(numStr) * 100);
-            this.setState({amount: amount, numDecimals})
+        if (this.state.isDollarOrder) {
+            this.handleDollarOrderChange(val);
         } else {
             allowableChars = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
             for(let i = 0; i < val.length; i++) {
@@ -167,12 +177,12 @@ class Sidebar extends React.Component {
         let trade = {};
         trade.ticker = this.props.ticker;
         trade.created_at = this.state.createdAt || new Date();
-        if (this.state.investIn === "Dollars") {
+        if (this.state.isDollarOrder) {
             trade.num_shares = Math.floor(this.state.amount / this.state.price)
         } else {
             trade.num_shares = this.state.numShares;
         }
-        if (this.state.order === "Sell") {
+        if (!this.state.isBuyOrder) {
             trade.num_shares *= -1;
         }
         trade.trade_price = this.state.price;
@@ -180,6 +190,8 @@ class Sidebar extends React.Component {
     }
 
     render() {
+        const {colorDark, color} = this.props;
+        const {isDollarOrder} = this.state;
         return (
             <form className="order-form" onSubmit={this.handleSubmit}>
                 <div className="buy-sell-sizer">
@@ -197,26 +209,26 @@ class Sidebar extends React.Component {
                 <div className="order-info-positioner">
                     <div>
                         <span>Invest In</span>
-                        <select className="invest-in-select" onChange={this.handleSelect}>
-                            <option value="Dollars" className={(this.props.investIn === "Dollars" ? "selected " : "") + (this.props.valueIncreased ? "green-background-selected" : "red-background-selected")}>Dollars</option>
-                            <option value="Shares" className={(this.props.investIn === "Dollars" ? "selected " : "") + (this.props.valueIncreased ? "green-background-selected" : "red-background-selected")}>Shares</option>
+                        <select className="invest-in-select" onChange={this.handleSelect} value={isDollarOrder ? "Dollars" : "Shares"}>
+                            <option value="Dollars" className={(isDollarOrder ? "selected " : "") + `${color}-background-selected`}>Dollars</option>
+                            <option value="Shares" className={(!isDollarOrder ? "selected " : "") + `${color}-background-selected`}>Shares</option>
                         </select>
                     </div>
                     <div>
-                        <span>{this.state.investIn === "Dollars" ? "Amount" : "Shares"}</span>
-                        <input onChange={this.handleChange} className={(this.state.investIn === "Dollars" ? "dollar-input " : "share-input ") + (this.props.valueIncreased ? "dark-green-border-focus" : "red-border-focus")} value={((this.state.investIn === "Dollars") ? (this.state.amount === "" ? "" : formatToDollar(this.state.amount, this.state.numDecimals)) : this.state.numShares)} onBlur={this.handleBlur} placeholder={(this.state.investIn === "Dollars" ? "$0.00" : "0")} type="text"/>
+                        <span>{isDollarOrder ? "Amount" : "Shares"}</span>
+                        <input onChange={this.handleChange} className={(isDollarOrder ? "dollar-input " : "share-input ") + `${colorDark}-border-focus`} value={(isDollarOrder ? (this.state.amount === "" ? "" : formatToDollar(this.state.amount, this.state.numDecimals)) : this.state.numShares)} onBlur={this.handleBlur} placeholder={(isDollarOrder ? "$0.00" : "0")} type="text"/>
                     </div>
                     <div>
-                        <span>Market Price (<button onClick={this.handleReset} className={this.props.valueIncreased ? "dark-green" : "red"}>reset</button>)</span>
+                        <span>Market Price (<button onClick={this.handleReset} className={colorDark}>reset</button>)</span>
                         <span className="order-form-dollar">{formatToDollar(this.state.price)}</span>
                     </div>
                     <div>
-                        <span>{this.state.investIn === "Dollars" ? "Est. Quantity" : "Est. Proceeds"}</span>
-                        <span>{this.state.investIn === "Dollars" ? Math.floor(this.state.amount / this.state.price) : (this.state.numShares === "-" ? formatToDollar(0) : formatToDollar(this.state.numShares * this.state.price))}</span>
+                        <span>{isDollarOrder ? "Est. Quantity" : "Est. Proceeds"}</span>
+                        <span>{isDollarOrder ? Math.floor(this.state.amount / this.state.price) : (this.state.numShares === "-" ? formatToDollar(0) : formatToDollar(this.state.numShares * this.state.price))}</span>
                     </div>
                 </div>
                 <div className="order-button-wrapper">
-                    <button className={"order-button " + (this.props.valueIncreased ? "green-background light-green-background-hover" : "red-background light-red-background-hover")}>Place Order</button>
+                    <button className={`order-button ${color}-background light-${color}-background-hover`}>Place Order</button>
                 </div>
             </form>
         )
