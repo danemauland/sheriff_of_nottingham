52dbdce8697e01a70ad3738c5304f670a619084b
dane <danemauland@gmail.com>
2021-01-13 11:16:10 -0500

    Refactor formatToDollar

--- frontend/util/dashboard_calcs.js
+++ frontend/util/dashboard_calcs.js
@@ -8,23 +8,14 @@ export const THREE_MONTH_OFFSET = 91;
 
 export const formatToDollar = (num, numDecimals = 2) => {
     if ((typeof num) !== "number") return false;
-    let trail = Math.floor(num % 100).toString();
-    if (trail.length < 2) trail += "0";
-    num = (num / 100).toLocaleString();
-    if (numDecimals === 2) {
-        if (num[num.length - 2] === ".") {num += trail[1]}
-        else if (num[num.length - 3] !== ".") {num += "." + trail};
-    } else if (numDecimals === 1) {
-        if (num[num.length - 3] === ".") {num = num.slice(0, -1)}
-        else if (num[num.length - 2] !== ".") {num += "." + trail[0]}
-    } else if (numDecimals === 0) {
-        if (num[num.length - 3] === ".") {num = num.slice(0, -3)}
-        else if (num[num.length - 2] === ".") {num = num.slice(0, -3)}
-    } else if (numDecimals === -1) {
-        if (num[num.length - 3] === ".") {num = num.slice(0, -3)}
-        else if (num[num.length - 2] === ".") {num = num.slice(0, -3)};
-        num += "."
-    }
+
+    num /= 100;
+
+    if (numDecimals < 1) num = num.toFixed(0);
+
+    if (numDecimals === 0) num += ".";
+    else if (numDecimals > 0) num = num.toFixed(numDecimals);
+    
     return "$" + num;
 }
 
