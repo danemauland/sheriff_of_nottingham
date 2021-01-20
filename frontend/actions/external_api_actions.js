fa556171090ca0b829f4cdae7a0b188764468500
dane <danemauland@gmail.com>
2021-01-20 12:25:49 -0500

    comment out market close day adjustment

--- frontend/actions/external_api_actions.js
+++ frontend/actions/external_api_actions.js
@@ -231,7 +231,7 @@ const getStartTime = () => { //daylight savings adjustment made, but it will be 
     startTime.setMinutes(30);
     startTime.setSeconds(0);
     startTime.setMilliseconds(0);
-    startTime.setUTCDate(startTime.getUTCDate() - 1); // COMMENT IN ON MARKET CLOSE DAYS
+    // startTime.setUTCDate(startTime.getUTCDate() - 1); // COMMENT IN ON MARKET CLOSE DAYS
     const day = startTime.getUTCDay();
     if (day === 6) {startTime.setUTCDate(startTime.getUTCDate() - 1)}
     else if (day === 0) { startTime.setUTCDate(startTime.getUTCDate() - 2)};
