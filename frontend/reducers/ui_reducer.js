616aaf5541d8024670a92249063e04d3c8efba48
dane <danemauland@gmail.com>
2021-01-26 09:09:10 -0500

    Prune UI slice of state

--- frontend/reducers/ui_reducer.js
+++ frontend/reducers/ui_reducer.js
@@ -1,16 +1,16 @@
 import {combineReducers} from "redux";
 import modalReducer from "./modal_reducer";
 import valueIncreasedReducer from "./value_increased_reducer";
-import chartSelectedReducer from "./chart_selected_reducer";
-import updatesReducer from "./updates_reducer";
-import loadingReducer from "./loading_reducer";
+// import chartSelectedReducer from "./chart_selected_reducer";
+// import updatesReducer from "./updates_reducer";
+// import loadingReducer from "./loading_reducer";
 import apiDebounceStartTimeReducer from "./api_debounce_start_time_reducer.js";
 
 export default combineReducers({
     modal: modalReducer,
     valueIncreased: valueIncreasedReducer,
-    chartSelected: chartSelectedReducer,
-    updatesNeeded: updatesReducer,
-    loading: loadingReducer,
+    // chartSelected: chartSelectedReducer,
+    // updatesNeeded: updatesReducer,
+    // loading: loadingReducer,
     apiDebounceStartTime: apiDebounceStartTimeReducer,
 })
